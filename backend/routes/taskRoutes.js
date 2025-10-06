8576dc1ecf90c53288144012075b5a4faf09e74b
Walter Arias Molino <walteram.academico@gmail.com>
2025-10-06 09:07:10 -0300

    feat

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -9,14 +9,4 @@ taskRouter.post("/api/tasks/:id", (req, res) => {
   res.status(201).json({ message: "Tarea creada" });
 });
 
-productosRouter.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const producto = productos.find((p) => p.id === id);
-  if (!producto) {
-    return res.status(404).json({ message: "Producto no encontrado." });
-  }
-
-  res.json(producto);
-});
-
 module.exports = taskRouter;
